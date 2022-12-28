19efd3eb8bee672b7ba9982c1eb36975e5870ab9
Priotosh <priotosh.mondal7@gmail.com>
2022-12-28 10:14:50 +0530

    Initial commit

---
+++ Medicam/views.py
@@ -0,0 +1,13 @@
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate,login
+from django.shortcuts import redirect
+from django.views.decorators.csrf import csrf_exempt
+
+import os
+
+def index(request):
+    return render(request,'index.html')
