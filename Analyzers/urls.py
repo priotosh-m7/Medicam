47b7670c88fcfe0267df3dbebb34d390fa538566
Priotosh <priotosh.mondal7@gmail.com>
2023-01-03 21:02:17 +0530

    designed top page of info section

--- Analyzers/urls.py
+++ Analyzers/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
-
+from django.contrib import admin
+from django.urls import path, include
+from  . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 app_name = 'polls'
@@ -7,4 +12,10 @@ urlpatterns = [
     path('brain', views.brainTumor, name='bt'),
     path('chest', views.chestXray, name='cx'),
     path('kidney', views.kidneyScans, name='ks'),
-]
+] + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
+
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL,
+                              document_root=settings.MEDIA_ROOT)
--- Medicam/urls.py
+++ Medicam/urls.py
@@ -16,9 +16,19 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from  . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index),
     path('analyzers/',include('Analyzers.urls')),
     path('knowledge/',views.info),
-]
+]+ static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
+
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL,
+                              document_root=settings.MEDIA_ROOT)
